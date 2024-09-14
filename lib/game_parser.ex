4b971e99958e6a572691fc518116edec60627913
akira utsuki <a.utsuki@gmail.com>
2024-09-14 15:25:46 +0900

    jsonファイルの保存

--- lib/game_parser.ex
+++ lib/game_parser.ex
@@ -6,8 +6,41 @@ defmodule GameParser do
   end
 
   defp parse_score(document) do
-    document
-    |> Floki.find("div#table_linescore")
+    top = parse_inning(document, "top")
+    bottom = parse_inning(document, "bottom")
+
+    %{
+      top: top,
+      bottom: bottom
+    }
+
+  end
+
+  defp parse_inning(document, attack) do
+    inning = document
+    |> Floki.find("tr.#{attack}")
+
+    name = inning
+    |> Floki.find("span.hide_sp")
+    |> Floki.text()
+
+    scores = inning
+    |> Floki.find("td")
+    |> Enum.filter(fn {_tag, attrs, _value} ->
+      not Enum.any?(attrs, fn{attr, _attr_val} -> attr == "class" end)
+    end )
+    |> Enum.map(fn {_tag,_attr,value} -> value end)
+
+
+    total = inning
+    |> Floki.find("td.total-1")
+    |> Floki.text()
+
+    %{
+      name: name,
+      scores: List.flatten(scores),
+      total: total
+    }
   end
 
 end
--- lib/npb_games_crawler.ex
+++ lib/npb_games_crawler.ex
@@ -3,33 +3,35 @@ defmodule NpbGamesCrawler do
   Documentation for `NpbGamesCrawler`.
   """
 
-  @game_url "https://npb.jp/scores/2024/0329/g-t-01/"
-
-  @base_url "https://npb.jp/games/"
+  @base_url "https://npb.jp"
   #@game_months ["03","04", "05","06","07","08","09","10","11"]
   @game_months ["03"]
 
+  #@game_url ["https://npb.jp/scores/2024/0329/g-t-01/"]
 
 
-  def crawl(_year \\ "2024" ) do
-    #schedules_links = NpbGamesCrawler.makeScoreLinks(year)
+  @spec crawl(any()) :: none()
+  def crawl(year \\ "2024" ) do
+    makeScoreLinks(year)
+    |> Enum.map(&fetch_game/1)
+    |> Enum.map(&save_json_to_file/1)
 
-
-    HTTPoison.get!("#{@game_url}").body
-    |> GameParser.parse()
   end
 
+  @spec makeScoreLinks(any()) :: list()
   def makeScoreLinks(year \\ "2024") do
     links = @game_months
-    |> Enum.map(&build_url(year, &1))
+    |> Enum.map(&build_schedule_url(year, &1))
     |> Enum.map(&fetch_schedule/1)
 
     List.flatten(links)
+    |> Enum.map(&build_game_url/1)
+    |> Enum.map(fn url -> {url,build_game_filepath(url)} end)
 
   end
 
-  defp build_url(year, month) do
-    "#{@base_url}#{year}/schedule_#{month}_detail.html"
+  defp build_schedule_url(year, month) do
+    "#{@base_url}/games/#{year}/schedule_#{month}_detail.html"
   end
 
   defp fetch_schedule(url) do
@@ -37,5 +39,55 @@ defmodule NpbGamesCrawler do
     |> HTTPoison.get!()
     |> Map.get(:body)
     |> ScheduleParser.parse()
+  end
+
+  def build_game_url(url) do
+    "#{@base_url}#{url}"
+  end
+
+  defp build_game_filepath(url) do
+    url
+    |> String.replace("https://npb.jp/", "")
+    |> String.trim_trailing("/")
+    |> Kernel.<>(".json")
+
+  end
+
+  defp fetch_game({url,path}) do
+    unless File.exists?(path) do
+      score = url
+      |> HTTPoison.get!()
+      |> Map.get(:body)
+      |> GameParser.parse()
+
+      {score, path}
+    end
+  end
+
+  def save_json_to_file(nil) do
+    IO.puts("取得済み.")
+  end
+  def save_json_to_file({data, file_path}) do
+    dir_path = Path.dirname(file_path)
+
+    # フォルダが存在しなければ作成
+    unless File.exists?(dir_path) do
+      case File.mkdir_p(dir_path) do
+        :ok -> :ok
+        {:error, reason} -> {:error, "Failed to create directory: #{reason}"}
+      end
+    end
+
+    # Elixirのデータ構造をJSON文字列に変換
+    case Jason.encode(data) do
+      {:ok, json_string} ->
+        # JSON文字列を指定したファイルに書き込み
+        case File.write(file_path, json_string) do
+          :ok -> {:ok, "File saved successfully!"}
+          {:error, reason} -> {:error, "Failed to save file: #{reason}"}
+        end
+      {:error, reason} ->
+        {:error, "Failed to encode JSON: #{reason}"}
+    end
   end
 end
